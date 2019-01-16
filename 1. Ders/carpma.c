e3414c56b1df28bd1a8c6ee3abb6ffa9fe617d0e
baraday <46758391+baraday@users.noreply.github.com>
2019-01-16 21:05:34 +0300

    Add files via upload
    First Program from C

---
+++ 1. Ders/carpma.c
@@ -0,0 +1,23 @@
+
+#include <stdio>
+
+int main ()
+{
+	int number_1;
+	int number_2;
+	
+	float answer;
+	
+	
+	scanf("%d", &number_1);
+	scanf("%d", &number_2);
+	
+	answer = number_1 * number_2;
+	
+	printf("Answer of two number is %d", &answer);
+	
+	return 0;
+	
+	
+	
+}
